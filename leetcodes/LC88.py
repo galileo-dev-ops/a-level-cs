d585625983a212cf07aad6ee973eea063e5ca55c
galileo-dev-ops <142727059+galileo-dev-ops@users.noreply.github.com>
2023-12-27 20:18:57 +0000

    -

--- leetcodes/LC88.py
+++ leetcodes/LC88.py
@@ -1,3 +1,6 @@
+## This is Leetcode problem #88 - Merge Sorted Array
+## https://leetcode.com/problems/merge-sorted-array/
+
 class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         i = j = 0
